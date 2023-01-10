1fa7c8c18290949a2f8a81411f9ba0103b4317f8
Akinyele Ibrahim <iakinyele3@gmail.com>
2023-01-10 21:59:31 +0100

    top to sign up

--- lib/home.dart
+++ lib/home.dart
@@ -12,8 +12,17 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  TextEditingController _nameContoller = TextEditingController();
+  TextEditingController _emailContoller = TextEditingController();
+  TextEditingController _passwordContoller = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
+    const outlineInputBorder = OutlineInputBorder(
+      borderRadius: BorderRadius.all(
+        Radius.circular(10),
+      ),
+    );
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
@@ -30,31 +39,137 @@ class _HomePageState extends State<HomePage> {
           child: SingleChildScrollView(
             child: Column(
               children: [
-                Container(
-                  alignment: Alignment.centerLeft,
-                  child: Image.asset("$img/back-icon.png"),
+                Column(
+                  children: [
+                    Container(
+                      alignment: Alignment.centerLeft,
+                      child: Image.asset("$img/back-icon.png"),
+                    ),
+                    const Text(
+                      "Register Account",
+                      style: TextStyle(
+                        fontSize: 30,
+                      ),
+                    ),
+                    Container(
+                      alignment: Alignment.center,
+                      margin: const EdgeInsets.symmetric(
+                          horizontal: 25, vertical: 10),
+                      child: const Text(
+                        "Fill Your Details Or Continue With\nSocial Media",
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ],
                 ),
-                Container(
-                  child: Column(
-                    children: [
-                      const Text(
-                        "Register Account",
-                        style: TextStyle(
-                          fontSize: 30,
-                          // fontWeight: FontWeight.bold,
+                Column(
+                  children: [
+                    // name textfield
+                    Container(
+                      alignment: Alignment.centerLeft,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            "Your Name",
+                            textAlign: TextAlign.start,
+                          ),
+                          Container(
+                            margin: const EdgeInsets.symmetric(vertical: 10),
+                            child: TextField(
+                              controller: _nameContoller,
+                              decoration: const InputDecoration(
+                                filled: true,
+                                fillColor: Color(0xffF7F7F9),
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(10),
+                                  ),
+                                ),
+                                hintText: "xxxxxxxx",
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    // email textfield
+                    Container(
+                      alignment: Alignment.centerLeft,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            "Email Address  ",
+                            textAlign: TextAlign.start,
+                          ),
+                          Container(
+                            margin: const EdgeInsets.symmetric(vertical: 10),
+                            child: TextField(
+                              controller: _emailContoller,
+                              decoration: const InputDecoration(
+                                filled: true,
+                                fillColor: Color(0xffF7F7F9),
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(10),
+                                  ),
+                                ),
+                                hintText: "xyz@gmail.com",
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    // password textfield
+                    Container(
+                      alignment: Alignment.centerLeft,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            "Password",
+                            textAlign: TextAlign.start,
+                          ),
+                          Container(
+                            margin: const EdgeInsets.symmetric(vertical: 10),
+                            child: TextField(
+                              obscureText: true,
+                              controller: _passwordContoller,
+                              decoration: const InputDecoration(
+                                  filled: true,
+                                  fillColor: Color(0xffF7F7F9),
+                                  border: outlineInputBorder,
+                                  hintText: "password",
+                                  suffixIcon:
+                                      Icon(Icons.no_encryption_outlined)),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    InkWell(
+                      onTap: () {},
+                      child: Container(
+                        width: double.infinity,
+                        padding: const EdgeInsets.all(15),
+                        decoration: const BoxDecoration(
+                          color: Colors.blue,
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(10),
+                          ),
+                        ),
+                        child: const Text(
+                          "Sign Up",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
                         ),
                       ),
-                      Container(
-                        alignment: Alignment.center,
-                        margin: const EdgeInsets.symmetric(
-                            horizontal: 25, vertical: 10),
-                        child: const Text(
-                          "Fill Your Details Or Continue With Social Media",
-                          textAlign: TextAlign.center,
-                        ),
-                      )
-                    ],
-                  ),
+                    )
+                  ],
                 ),
               ],
             ),
